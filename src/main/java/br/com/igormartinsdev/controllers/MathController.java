c85bb239478c8698c421b4605c935b1c154c416f
Igor Martins <dev.igor.martins@gmail.com>
2025-08-27 01:10:01 -0300

    relembrando API's

---
+++ src/main/java/br/com/igormartinsdev/controllers/MathController.java
@@ -0,0 +1,36 @@
+package br.com.igormartinsdev.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception{
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new IllegalAccessException();
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+
+    }
+
+    private boolean isNumeric(String strNumber){
+        if(strNumber == null || strNumber.isEmpty())
+            return false;
+
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    private Double convertToDouble(String strNumber){
+        String number = strNumber.replace(",", ".");
+
+        return Double.parseDouble(number);
+    }
+
+}
