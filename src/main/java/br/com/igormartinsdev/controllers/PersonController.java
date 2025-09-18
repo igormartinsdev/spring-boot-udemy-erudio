151ac11403eb445c1952c51e7d9a324d7737909a
Igor Martins <dev.igor.martins@gmail.com>
2025-09-18 00:45:02 -0300

    método salvar no controller

--- src/main/java/br/com/igormartinsdev/controllers/PersonController.java
+++ src/main/java/br/com/igormartinsdev/controllers/PersonController.java
@@ -4,10 +4,7 @@ import br.com.igormartinsdev.model.Person;
 import br.com.igormartinsdev.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -28,6 +25,11 @@ public class PersonController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return service.create(person);
     }
 
 }
