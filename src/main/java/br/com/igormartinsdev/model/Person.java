1672cbb5d09526b1cfa9ab87952618bc4c0a2d8c
Igor Martins <dev.igor.martins@gmail.com>
2025-10-15 00:30:34 -0300

    mapeamento da classe person

--- src/main/java/br/com/igormartinsdev/model/Person.java
+++ src/main/java/br/com/igormartinsdev/model/Person.java
@@ -1,8 +1,12 @@
 package br.com.igormartinsdev.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -10,10 +14,20 @@ public class Person implements Serializable {
     public Person() {
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String fisrtName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Long getId() {
