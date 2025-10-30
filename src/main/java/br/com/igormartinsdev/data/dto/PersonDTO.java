cd07a3cb1ba4355cf73606fcbc3ac4d8091d4bbb
Igor Martins <dev.igor.martins@gmail.com>
2025-10-30 01:16:15 -0300

    - adicionando a dependência do Dozer mapper para afins de estudos mas que depois será substituído - DTO da classe Person - configuração do dozer mapper no projeto

---
+++ src/main/java/br/com/igormartinsdev/data/dto/PersonDTO.java
@@ -0,0 +1,70 @@
+package br.com.igormartinsdev.data.dto;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public PersonDTO() {
+    }
+
+    private Long id;
+    private String fisrtName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFisrtName() {
+        return fisrtName;
+    }
+
+    public void setFisrtName(String fisrtName) {
+        this.fisrtName = fisrtName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(id, person.id) && Objects.equals(fisrtName, person.fisrtName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fisrtName, lastName, address, gender);
+    }
+}
